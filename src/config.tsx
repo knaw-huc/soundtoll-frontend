b76f8cfa71118db725e95a994378b60bb3875b32
Rob Zeeman <this_is_not_an_email-3cc036843bde09c86580da2d3d753a527d1e8bfa@authentication.clariah.nl>
2020-09-14 23:00:08 +0200

    distinctive dropdown standard port names, smaller font

--- src/config.tsx
+++ src/config.tsx
@@ -1,2 +1,2 @@
-export const SONT_SERVICE: string = "/sont_service/";
+export const SONT_SERVICE: string = "http://www.huc.localhost/sont_service/";
 export const ELASTIC_URL: string = SONT_SERVICE + "elastic/search/";
